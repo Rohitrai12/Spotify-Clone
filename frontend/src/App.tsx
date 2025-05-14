358fe7980e869364d7bd9af340fd5dd9b9fb566c
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-05-14 15:31:44 +0500

    Shadcn Ui added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
 
+      <div className="flex flex-col items-center justify-center min-h-svh">
+        <Button>Click me</Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
