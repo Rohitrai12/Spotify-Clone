e529016ce086111c0b2f96fd8d410e4b5ba83185
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-05-14 16:22:57 +0500

    Song Model created

---
+++ backend/src/models/song.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema(
+  {
+    title: {
+      required: true,
+      type: String,
+    },
+    artist: {
+      required: true,
+      type: String,
+    },
+    imageURL: {
+      required: true,
+      type: String,
+    },
+    audioURL: {
+      required: true,
+      type: String,
+    },
+    durationURL: {
+      required: Number,
+      type: String,
+    },
+    albumId: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Album",
+        required:false
+    },
+  },
+  { timeseries: true }
+);
+
+const Song = mongoose.model("Song", songSchema);
+
+export default Song;
