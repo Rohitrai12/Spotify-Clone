b81a047941244f75d5f12fc1dedaf7c2dbc1048b
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-05-15 15:43:55 +0500

    getAllSong and getFeaturedSongs  Endpoint Added

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { getAllSong, getFeaturedSongs } from "../controllers/songs.controller.js";
+import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
 
 const SongsRouter = Router()
+
+SongsRouter.get("/getAllSongs", protectRoute, requireAdmin, getAllSong)
+SongsRouter.get("/featured", getFeaturedSongs)
+
 
 export default SongsRouter
