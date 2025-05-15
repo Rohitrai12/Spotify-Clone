12ee5ac6f9071d4ba2c3210b0efb28c06239ecb8
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-05-15 15:31:13 +0500

    Album Routes and Controllers

--- backend/src/routes/album.route.js
+++ backend/src/routes/album.route.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { getAllAlbumById, getAllAlbums } from "../controllers/album.controller.js";
 
 const AlbumRouter = Router()
+
+AlbumRouter.get("/", getAllAlbums)
+AlbumRouter.get("/:albumId", getAllAlbumById)
 
 export default AlbumRouter
