3b1117169c725c3d903993f048bdc2064eb9c273
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-05-15 15:46:11 +0500

    get Made ForYou Songs API added

--- backend/src/routes/songs.route.js
+++ backend/src/routes/songs.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getAllSong, getFeaturedSongs } from "../controllers/songs.controller.js";
+import { getAllSong, getFeaturedSongs, getTrendingSongs, getMadeForYouSongs } from "../controllers/songs.controller.js";
 import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
 
 const SongsRouter = Router()
 
 SongsRouter.get("/getAllSongs", protectRoute, requireAdmin, getAllSong)
 SongsRouter.get("/featured", getFeaturedSongs)
-
+SongsRouter.get("/made-for-you", getMadeForYouSongs)
+SongsRouter.get("/trending", getTrendingSongs)
 
 export default SongsRouter
