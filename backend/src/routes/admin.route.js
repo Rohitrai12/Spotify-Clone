936eaf11122591bed0bac3fc6b474adf4b2736a2
Rohit Rai <161055240+Rohitrai12@users.noreply.github.com>
2025-05-15 14:57:18 +0500

    Delete song API created

--- backend/src/routes/admin.route.js
+++ backend/src/routes/admin.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { protectRoute, requireAdmin } from "../middlewares/auth.middleware.js";
-import {createSong} from '../controllers/admin.controller.js'
+import {createSong, deleteSong} from '../controllers/admin.controller.js'
 
 const AdminRouter = Router()
 
-AdminRouter.get("/", protectRoute, requireAdmin, createSong)
+AdminRouter.post("/", protectRoute, requireAdmin, createSong)
+AdminRouter.delete("/songs/:id", protectRoute, requireAdmin, deleteSong)
 
 export default AdminRouter
